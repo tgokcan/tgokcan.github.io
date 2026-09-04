76c972a6a29ad323642894eab4f049a34d59a416
tgokcan <tolga.bektas@baun.edu.tr>
2026-09-04 15:20:07 +0300

    CD için bir kaç düzenleme.

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,11 @@ const withMDX = mdx({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
